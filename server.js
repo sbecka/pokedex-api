5dcfeac72b2d932b7b17809d6fd07d7ee8cad339
Sabrina Becker <sbecka8@gmail.com>
2020-01-27 22:42:33 -0500

    Add pokemon data and get requests for server endpoints type and pokemon

--- server.js
+++ server.js
@@ -1,32 +1,57 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
 
 console.log(process.env.API_TOKEN);
 
 const app = express();
-
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
 app.use(function validateBearerToken(req, res, next) {
     console.log('validate bearer token middleware');
-    //mpve to thr next middleware
+    //const bearerToken = req.get('Authorization').split(' ')[1];
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    //move to thr next middleware
     next();
 })
 
-function handleGetTypes(req, res) {
+app.get('/types', function handleGetTypes(req, res) {
     res.json(validTypes);
-};
+    }
+);
 
-app.get('/types', handleGetTypes);
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    
+    const { name, type } = req.query;
+    let response = POKEDEX.pokemon;
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!');
-};
+    const searchName = name.toLowerCase();
 
-app.get('/pokemon', handleGetPokemon);
+    if (name) {
+        response = response.filter(pokemon => 
+        pokemon.name.toLowerCase().includes(searchName));
+    }
+    
+    if (type) {
+        response = response.filter(pokemon => 
+            pokemon.type.includes(type));
+    }
+
+    res.json(response);
+    }
+);
 
 const PORT = 8000;
 
